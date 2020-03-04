a8075b2e3fd3798feb338142864f53c1928d1ac6
Sergii Liebodkin <Sergii.Liebodkin@halliburton.com>
2020-03-04 17:35:37 +0200

    send to nothere

--- src/js/TextureML/Types/SessionInfo.ts
+++ src/js/TextureML/Types/SessionInfo.ts
@@ -8,38 +8,32 @@ export class SessionInfo {
     // events
     public onload: (this: SessionInfo, sessionInfo: SessionInfo) => any = null;
 
+    // saveToJsonString
+    public saveToJsonString(): string {
+        // generate request data
+        let data = {
+            sessionId: this.sessionID,
+            coreLogs: {},
+            coreImageList: {},
+            cropImageList: {},
+            reprImageNames: this.sessionData.reprImageNames
+        };
+        this.sessionData.coreLogs.saveToJson(data.coreLogs);
+        this.sessionData.coreImageList.saveToJson(data.coreImageList);
+        this.sessionData.cropImageList.saveToJson(data.cropImageList);
+        return JSON.stringify(data);
+    }
+
     // send
     public send(): Promise<SessionInfo> {
         return new Promise<SessionInfo>((resolve, reject) => {
-            // generate request data
-            let dataJSON = {
-                sessionId: this.sessionID,
-                coreLogs: {},
-                coreImageList: {},
-                cropImageList: {},
-                reprImageNames: this.sessionData.reprImageNames
-            };
-            this.sessionData.coreLogs.saveToJson(dataJSON.coreLogs);
-            this.sessionData.coreImageList.saveToJson(dataJSON.coreImageList);
-            this.sessionData.cropImageList.saveToJson(dataJSON.cropImageList);
-            console.log(JSON.stringify(dataJSON));
-            
-            /*
-            gImageInfoList.forEach(imageInfo => dataJSON.payload.images[imageInfo.fileRef.name] = imageInfo.canvasImage.toDataURL().replace("data:image/png;base64,", ""));
             // create http request
-            let url = POST_URL + "/texml_v3";
             let xhr = new XMLHttpRequest();
-            xhr.open("POST", url, true);
+            xhr.open("POST", "", true);
             xhr.setRequestHeader("Content-Type", "application/json");
-            xhr.onreadystatechange = event => {
-                if (xhr.readyState === 4 && xhr.status === 200)
-                    resolve(xhr.responseText);
-            }
-            xhr.onerror = event => reject(xhr.statusText);
-            xhr.send(JSON.stringify(dataJSON));
-            // log message
-            gOverlayLog.addMessage(`Images sended to ${POST_URL} ...`);
-            */
+            xhr.onreadystatechange = event => { if (xhr.readyState === 4 && xhr.status === 200) resolve(this); };
+            xhr.onerror = event => reject(this);
+            xhr.send(this.saveToJsonString());
         })
     }
 }
